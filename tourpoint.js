cd82f1040b1a7bf658d521cb34fcfb80225d9501
hearteieiei <158025710+hearteieiei@users.noreply.github.com>
2024-05-21 22:24:56 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pattani CenTral Mosque ",
+          "type": "Cultural",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            101.25562104588107,
+            6.861864864761287
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Krue-Se Mosque",
+          "type": "Cultural",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            101.30317867812158,
+            6.872872601550185
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Lak Mueang",
+          "type": "Cultural",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            101.24398600000166,
+            6.880071824615754
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Na Pradu",
+          "type": "Cultural",
+          "level": 4.5
+        },
+        "geometry": {
+          "coordinates": [
+            101.14707399999952,
+            6.675525212876906
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Namtok Sai Khao National Park",
+          "type": "Natural",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+            101.11165027116294,
+            6.675392868974583
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Panare Beach",
+          "type": "Natural",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            101.49185999999969,
+            6.87041017342537
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Talo Kapo Beach",
+          "type": "Natural",
+          "level": 4.1
+        },
+        "geometry": {
+          "coordinates": [
+            101.37112299999916,
+            6.899042521115646
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wasukri Beach",
+          "type": "Natural",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            101.63852345767452,
+            6.708373179883239
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Somdet Phra Sinagarin Pattani Park",
+          "type": "Special",
+          "level": 4.4
+        },
+        "geometry": {
+          "coordinates": [
+            101.24557637544467,
+            6.892519150828704
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palas Market",
+          "type": "Special",
+          "level": 4.1
+        },
+        "geometry": {
+          "coordinates": [
+            101.47998027116529,
+            6.757433523637729
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
